4b6918db250e76854d3f8ba96a9822fa9c431161
Guilherme Rodrigues <guilherme.rodrigues@sabium.com.br>
2018-04-25 13:23:10 -0300

    jokenpo funfando

--- App.js
+++ App.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import styled from 'styled-components'
 
-export default class App extends React.Component {
-
-  constructor(props){
-    super(props)
-
-    this.state = {escolhaUsuario: ''}
-  }
-
-  setEscolhaUsuario(escolha){
-    this.setState({escolhaUsuario: escolha})
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Escolha do computador</Text>
-        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
-        <Button 
-          title="Pedra"
-          onPress = {()=>{this.setEscolhaUsuario("Pedra")}}
-          />
-        <Button 
-          title="Papel"
-          onPress = {()=>{this.setEscolhaUsuario("Papel")}}
-        />
-        <Button
-         title="Tesoura"
-         onPress = {()=>{this.setEscolhaUsuario("Tesoura")}}
-         />
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -44,3 +11,74 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   }
 });
+
+const ButtonsMargin = styled.View`
+  margin-top: 20;
+`
+
+const opcoesJokenPo = ['pedra', 'papel', 'tesoura']
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {escolhaUsuario: '', escolhaComputador: '', resultado: ''}
+  }
+
+  jokenpo(escolha){
+    let sorteio = Math.floor(Math.random() * 3)
+
+    let resultado = ''
+
+    switch(escolha){
+      case 'pedra':
+        resultado = opcoesJokenPo[sorteio] == 'tesoura' && opcoesJokenPo[sorteio] != 'pedra' ? 'Você venceu' : 'Computador Venceu'
+      break
+      case 'papel':
+        resultado = opcoesJokenPo[sorteio] == 'tesoura' && opcoesJokenPo[sorteio] != 'papel' ? 'Computador Venceu' : 'Você venceu'
+      break
+      case 'tesoura':
+        resultado = opcoesJokenPo[sorteio] == 'papel' && opcoesJokenPo[sorteio] != 'tesoura' ? 'Você venceu' : 'Computador Venceu'
+      break
+    }
+
+    this.setState({
+      escolhaUsuario: escolha, 
+      escolhaComputador:opcoesJokenPo[sorteio],
+      resultado: opcoesJokenPo[sorteio] == escolha ? 'Empate' : resultado
+    })
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{marginTop: 10}}>Computador: {this.state.escolhaComputador}</Text>
+        <Text style={{marginTop: 10}}>Você: {this.state.escolhaUsuario}</Text>
+        <Text style={{marginTop: 10}}>Resultado: {this.state.resultado}</Text>
+        <ButtonsMargin>
+          <Button 
+            title="pedra"
+            onPress = {()=>{this.jokenpo("pedra")}}
+            />
+        </ButtonsMargin>
+        <ButtonsMargin>
+          <Button 
+            title="papel"
+            onPress = {()=>{this.jokenpo("papel")}}
+            color="#841584"
+          />
+        </ButtonsMargin>
+        <ButtonsMargin>
+          <Button
+          title="tesoura"
+          onPress = {()=>{this.jokenpo("tesoura")}}
+          />
+        </ButtonsMargin>
+      </View>
+    );
+  }
+}
+
+
